1311ac2e373c74f41b629a0b236b2da83e6fb485
Ricardo Colzani <6742811+rcolzani@users.noreply.github.com>
2021-02-17 14:50:27 -0300

    add google analytics

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Calculator from './pages/Calculator';
 
 export default function Routes() {
+
+  useEffect(() => {
+    const trackingId = "G-RXJQKXQNK1"; // Replace with your Google Analytics tracking ID
+    ReactGA.initialize(trackingId);
+  }, []);
+
+
   return (
     <BrowserRouter>
       <Switch>
